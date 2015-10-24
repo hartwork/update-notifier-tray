c63aaeb5aa2a36b9a9a2fa54594de0a99690de74
Sebastian Pipping <sebastian@pipping.org>
2015-10-24 22:22:08 +0200

    Bump version to 1.2

--- update_notifier_tray/version.py
+++ update_notifier_tray/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2014 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (1, 1)
+VERSION = (1, 2)
 VERSION_STR = '.'.join([str(e) for e in VERSION])
